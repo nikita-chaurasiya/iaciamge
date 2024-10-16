d20f3cb4fd27e3404e979aa38b3584f934931593
nikita-chaurasiya <nikita.chaurasiya@gmail.com>
2024-10-16 18:34:09 +0530

    Delete php/mysql directory

--- php/mysql/insertvalues.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO natwest (firstname, rollnumber) VALUES ('Ali',1);
-INSERT INTO natwest (firstname, rollnumber) VALUES ('Jamie',2);
-INSERT INTO natwest (firstname, rollnumber) VALUES ('Kalyan',3);
-INSERT INTO natwest (firstname, rollnumber) VALUES ('Nikita',4);
-INSERT INTO natwest (firstname, rollnumber) VALUES ('Veera',5);
